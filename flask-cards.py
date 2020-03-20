fd9a642e8fb8d6bcaef11a592fa2454f8979da2c
Antoni Szczepanik <antoni.szczepanik@greensteam.com>
2020-03-20 11:15:20 +0100

    Fully working card drawing app :)

--- flask-cards.py
+++ flask-cards.py
@@ -1,30 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, emit, send
 
-from random import randint
-
-suits = {
-    0: 'Pik',
-    1: 'Karo',
-    2: 'Kier',
-    3: 'Trefl'
-}
-
-cards = {
-    0: 'Ass',
-    1: '2',
-    2: '3',
-    3: '4',
-    4: '5',
-    5: '6',
-    6: '7',
-    7: '8',
-    8: '9',
-    9: '10',
-    10: 'Dupek',
-    11: 'Królowa',
-    12: 'Król'
-}
+from random import randint, choice
 
 # initialize Flask
 app = Flask(__name__)
@@ -35,23 +12,44 @@ def index():
     """Serve the index HTML"""
     return render_template('index.html')
 
-@socketio.on('create')
-def on_create():
+@socketio.on('connect')
+def on_connect():
     """Create a game lobby"""
     room = 1
     join_room(room)
-    emit('join_room', {'room': room})
 
 @socketio.on('draw_card')
 def on_draw_card():
     card = draw_card()
-    socketio.emit('newcard', {'card': card})
+    socketio.emit('new_card', {'card': card}, broadcast=True)
 
 
 def draw_card():
+    suits = {
+        0: 'Pik',
+        1: 'Karo',
+        2: 'Kier',
+        3: 'Trefl'
+    }
+    cards = {
+        0: 'Ass',
+        1: '2',
+        2: '3',
+        3: '4',
+        4: '5 ',
+        5: '6',
+        6: '7',
+        7: '8',
+        8: '9',
+        9: '10',
+        10: 'Dupek',
+        11: 'Królowa',
+        12: 'Król'
+    }
+    emojis = ['😭', '😇',  '🤠', '🤡',  '🥳 ', '🤫', '🤭']
     x = randint(0,3) #random integer 0 to 3 to pick suit
     y = randint(0,12) #random integer 0 to 12 to pick card
-    return f"{cards[y]} {suits[x]}"
+    return f"{cards[y]} {suits[x]} {choice(emojis)}"
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
