71da0c97702de75294c4db52b6aa74547c9c890a
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2021-03-11 16:37:52 +0100

    Do not use reserved port

--- flask-cards.py
+++ flask-cards.py
@@ -50,4 +50,4 @@ def draw_card():
     return f"{cards[y]} {suits[x]}"
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port='80', debug=True)
+    socketio.run(app, host='0.0.0.0', port='1234', debug=True)
